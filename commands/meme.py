cec816e46137101dd54dad8d737e242dbdfa4945
Phantom <phantom@phantom-games.com>
2021-11-27 12:08:32 -0800

    Improved meme object functionality

--- commands/meme.py
+++ commands/meme.py
@@ -1,7 +1,8 @@
 import random
+from twitchio.ext import commands
 
 class MemeResponse:
-    def __init__(self, memeFilename):
+    def __init__(self, memeFilename: str):
         self.responses = []
         self.memeFile = f"./commands/resources/{memeFilename}.txt"
         with open(self.memeFile, 'r', encoding="utf-8") as txt_file:
@@ -15,10 +16,18 @@ class MemeResponse:
             for meme in self.responses:
                 txt_file.write(f"{meme}\n")
 
-    def add_response(self, response):
+    def add_response(self, response: str):
         self.responses.append(response)
         self.save_memes()
 
-    def get_random_response(self):
-        id = random.randrange(len(self.responses))
-        return self.responses[id]
+    def get_seeded_response(self, seed: int) -> str:
+        if len(self.responses) > 0:
+            id = seed % len(self.responses)
+            return self.responses[id]
+        return None
+
+    def get_random_response(self) -> str:
+        if len(self.responses) > 0:
+            id = random.randrange(len(self.responses))
+            return self.responses[id]
+        return None
