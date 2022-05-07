3531d756aa4489e13c6b869afab8ccfd0c7c5486
Phantom <phantom@phantom-games.com>
2022-05-07 00:07:15 -0700

    Feeding some low state_size sentences back into the generator with a higher state_size

--- commands/markov.py
+++ commands/markov.py
@@ -4,10 +4,13 @@ class MarkovHandler:
     def __init__(self):
         with open("./commands/resources/markov.txt", "r") as f:
             text = f.read()
-            self.text_model = markovify.NewlineText(text)
+            # state_size=1 is complete nonsense, 2 makes more ... real sentences, 3 is not random enough
+            state_size = 2
+            self.text_model = markovify.NewlineText(text, state_size=state_size)
             self.text_model.compile(inplace=True)
 
             print(self.text_model.to_dict()["state_size"])
+            print(f"Possible starting words: {[key[state_size - 1] for key in self.text_model.chain.model.keys() if '___BEGIN__' in key]}")
     
     def getMarkovString(self):
         output = self.text_model.make_sentence(tries=200)
