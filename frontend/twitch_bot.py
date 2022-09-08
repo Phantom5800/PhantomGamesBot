4c84938475da51239f2eb1aa7872d731e04b7570
Phantom <phantom@phantom-games.com>
2022-09-07 20:08:40 -0700

    Command updates

--- frontend/twitch_bot.py
+++ frontend/twitch_bot.py
@@ -162,8 +162,11 @@ class PhantomGamesBot(commands.Bot):
                 else:
                     await super().event_message(message)
 
-                    # save twitch messages that are not commands and contain at least two words
-                    if self.markov_data_store and not message.content.startswith(os.environ['BOT_PREFIX']) and len(set(message.content.split())) >= self.markov_store_minlen:
+                    # save twitch messages that are not commands, links and meet a minimum length requirement
+                    contains_link = "https://" in message.content
+                    not_self_post = not message.content.startswith(os.environ['BOT_PREFIX'])
+                    length_req = len(set(message.content.split())) >= self.markov_store_minlen
+                    if self.markov_data_store and not contains_link and not_self_post and length_req:
                         with open("./commands/resources/markov.txt", "a+") as f:
                             try:
                                 f.write(f"{message.content}\n")
