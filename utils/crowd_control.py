a1208c90d05723011ea26c3d58284786c40cc699
Phantom <phantom@phantom-games.com>
2025-06-22 13:46:15 -0700

    Disable heart blocks, save blocks, speedy spin and enable ohko mode

--- utils/crowd_control.py
+++ utils/crowd_control.py
@@ -11,14 +11,22 @@ pm64_bit_values = {
     55:     "Set FP 0",
     60:     "Toggle Mirror Mode",
     75:     "Shuffle Current Seed", # change seed being played
+    80:     "Disable Speedy Spin", # 1 minute
     100:    "Slow Go", # adds one minute to the queue
-    200:    "Disable All Badges"
+    200:    "Disable All Badges",
+    220:    "Disable Heart Blocks", # 5 minutes
+    250:    "Disable Save Blocks", # 5 minutes
+    1000:   "OHKO Mode" # 5 minutes
 }
 
 cc_root = "./commands/resources/crowdcontrol"
 cc_multiplier = int(os.environ.get("CC_MULTIPLIER", "1"))
-slowgo_queue = 0
-random_pitch_queue = 0
+slowgo_queue        = 0
+random_pitch_queue  = 0
+speedy_queue        = 0
+heart_block_queue   = 0
+save_block_queue    = 0
+ohko_queue          = 0
 
 def set_cc_multiplier(mult: int):
     if mult != 0:
@@ -81,14 +89,45 @@ def handle_pm64_cc_bits(bits):
             print("[CC] Toggle Mirror Mode")
             with open(f"{cc_root}/pm64r-toggle-mirror.txt", "w+") as f:
                 f.write("eww")
+        elif pm64_bit_values[bits] == "Disable Speedy Spin":
+            print("[CC] Disable Speedy Spin")
+            with open(f"{cc_root}/pm64r-disable-speedy.txt", "w+") as f:
+                f.write("eww")
+            speedy_queue += 60
+        elif pm64_bit_values[bits] == "Disable Heart Blocks":
+            print("[CC] Disable Heart Blocks")
+            with open(f"{cc_root}/pm64r-disable-heart-blocks.txt", "w+") as f:
+                f.write("eww")
+            heart_block_queue += 300
+        elif pm64_bit_values[bits] == "Disable Save Blocks":
+            print("[CC] Disable Save Blocks")
+            with open(f"{cc_root}/pm64r-disable-save-blocks.txt", "w+") as f:
+                f.write("eww")
+            save_block_queue += 300
+        elif pm64_bit_values[bits] == "OHKO Mode":
+            print("[CC] OHKO Mode")
+            with open(f"{cc_root}/pm64r-ohko.txt", "w+") as f:
+                f.write("eww")
+            ohko_queue += 300
+
 
 
 def handle_pm64_cc_periodic_update(seconds):
-    slowgo_queue = max(slowgo_queue - seconds, 0)
-    random_pitch_queue = max(random_pitch_queue - seconds, 0)
+    update_time = lambda a : max(a - seconds, 0)
+
+    slowgo_queue = update_time(slowgo_queue)
+    random_pitch_queue = update_time(random_pitch_queue)
+    speedy_queue = update_time(speedy_queue)
+    heart_block_queue = update_time(heart_block_queue)
+    save_block_queue = update_time(save_block_queue)
+    ohko_queue = update_time(ohko_queue)
 
     slowgo_file = f"{cc_root}/pm64r-slowgo.txt"
     pitch_file = f"{cc_root}/pm64r-random-pitch.txt"
+    speedy_file = f"{cc_root}/pm64r-disable-speedy.txt"
+    heart_block_file = f"{cc_root}/pm64r-disable-heart-blocks.txt"
+    save_block_file = f"{cc_root}/pm64r-disable-save-blocks.txt"
+    ohko_file = f"{cc_root}/pm64r-ohko.txt"
 
     if slowgo_queue <= 0 and os.path.isfile(slowgo_file):
         print("[CC] Slow Go no longer enabled")
@@ -97,3 +136,19 @@ def handle_pm64_cc_periodic_update(seconds):
     if random_pitch_queue <= 0 and os.path.isfile(pitch_file):
         print("[CC] Random Pitch no longer enabled")
         os.remove(pitch_file)
+
+    if speedy_queue <= 0 and os.path.isfile(speedy_file):
+        print("[CC] Speedy Spin is no longer disabled")
+        os.remove(speedy_file)
+
+    if heart_block_queue <= 0 and os.path.isfile(heart_block_file):
+        print("[CC] Heart Blocks no longer disabled")
+        os.remove(heart_block_file)
+
+    if save_block_queue <= 0 and os.path.isfile(save_block_file):
+        print("[CC] Save Blocks no longer disabled")
+        os.remove(save_block_file)
+
+    if ohko_queue <= 0 and os.path.isfile(ohko_file):
+        print("[CC] OHKO Mode no longer enabled")
+        os.remove(ohko_file)
