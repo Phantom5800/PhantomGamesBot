22c1ee4ed275b1980e92696ae1aa5efaa6dd6b40
Phantom <phantom@phantom-games.com>
2025-06-22 02:08:11 -0700

    random pitch and disable all badges handling

--- utils/crowd_control.py
+++ utils/crowd_control.py
@@ -7,13 +7,16 @@ pm64_bit_values = {
     16:     "Subtract 10 Coins",
     20:     "Set HP 1",
     25:     "Set HP 2",
+    50:     "Random Pitch"
     55:     "Set FP 0",
-    100:    "Slow Go"
+    100:    "Slow Go",
+    200:    "Disable All Badges"
 }
 
 cc_root = "./commands/resources/crowdcontrol"
 cc_multiplier = int(os.environ.get("CC_MULTIPLIER", "1"))
 slowgo_queue = 0
+random_pitch_queue = 0
 
 def set_cc_multiplier(mult: int):
     if mult != 0:
@@ -57,10 +60,29 @@ def handle_pm64_cc_bits(bits):
                 with open(slowgo_file, "w+") as f:
                     f.write("Of course")
             slowgo_queue += 60
+        elif pm64_bit_values[bits] == "Random Pitch":
+            pitch_file = f"{cc_root}/pm64r-random-pitch.txt"
+            if not(os.path.isfile(slowgo_file)):
+                print("[CC] Random Pitch enabled")
+                with open(pitch_file, "w+") as f:
+                    f.write("why??")
+            random_pitch_queue += 60
+        elif pm64_bit_values[bits] == "Disable All Badges":
+            print("[CC] Disable All Badges")
+            with open(f"{cc_root}/pm64r-disablebadges.txt", "w+") as f:
+                f.write("Rude af")
 
 def handle_pm64_cc_periodic_update(seconds):
     slowgo_queue = max(slowgo_queue - seconds, 0)
+    random_pitch_queue = max(random_pitch_queue - seconds, 0)
+
     slowgo_file = f"{cc_root}/pm64r-slowgo.txt"
+    pitch_file = f"{cc_root}/pm64r-random-pitch.txt"
+
     if slowgo_queue <= 0 and os.path.isfile(slowgo_file):
         print("[CC] Slow Go no longer enabled")
         os.remove(slowgo_file)
+
+    if random_pitch_queue <= 0 and os.path.isfile(pitch_file):
+        print("[CC] Random Pitch no longer enabled")
+        os.remove(pitch_file)
