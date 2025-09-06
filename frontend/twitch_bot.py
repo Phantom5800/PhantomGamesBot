6ef5e9ab00aa59d46de194f0a6441c2c52ff75c5
Phantom <phantom@phantom-games.com>
2025-09-06 00:06:32 -0700

    more correct !commands

--- frontend/twitch_bot.py
+++ frontend/twitch_bot.py
@@ -290,7 +290,7 @@ class PhantomGamesBot(commands.Bot):
         # cannot actually append the custom list because the message is too long
         #command_list.extend(self.custom.get_command_list())
         command_list.sort()
-        await ctx.send(f"Built in commands: {command_list}. Custom commands: https://github.com/Phantom5800/PhantomGamesBot/blob/master/commands/resources/custom_commands.json")
+        await ctx.send(f"Built in commands: {command_list}. Custom commands: https://github.com/Phantom5800/PhantomGamesBot/blob/master/commands/resources/channels/{ctx.message.channel.name}/custom_commands.json")
 
     '''
     Add a custom command through twitch chat.
